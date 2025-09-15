6ce256bbb8ca90d97e23e762b2b38d5da418914d
bareera-fatima <bareerafatima03@gmail.com>
2025-09-15 08:08:06 -0700

    setup fixes

--- src/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-import db from '../config/db.js';
-
-export const getUsers = (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
-    if (err) return res.status(500).json({ error: err });
-    res.json(results);
-  });
-};
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// Register routes
+
 app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
